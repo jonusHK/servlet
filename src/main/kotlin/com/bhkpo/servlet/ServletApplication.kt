920a77f943daad16c98974c5086330e995180088
jonusHK <positipman@gmail.com>
2023-04-11 23:25:22 +0900

    Add: 서블릿 커스텀

--- src/main/kotlin/com/bhkpo/servlet/ServletApplication.kt
+++ src/main/kotlin/com/bhkpo/servlet/ServletApplication.kt
@@ -2,7 +2,9 @@ package com.bhkpo.servlet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
 
+@ServletComponentScan // 자동으로 서블릿 찾아서 등록
 @SpringBootApplication
 class ServletApplication
 
--- src/main/kotlin/com/bhkpo/servlet/basic/HelloServlet.kt
+++ src/main/kotlin/com/bhkpo/servlet/basic/HelloServlet.kt
@@ -1,4 +1,23 @@
 package com.bhkpo.servlet.basic
 
-class HelloServlet {
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+
+@WebServlet(name = "helloServlet", urlPatterns = ["/hello"])
+class HelloServlet: HttpServlet() {
+    // Tomcat 등 서블릿 컨테이너가 이 request, response 를 구현해서 전달함
+    override fun service(request: HttpServletRequest, response: HttpServletResponse) {
+        println("request = $request")
+        println("response = $response")
+
+        val username: String? = request.getParameter("username")
+        println("username = $username")
+
+        response.contentType = "text/plain"
+        response.characterEncoding = "utf-8"
+
+        response.writer?.write("hello $username")
+    }
 }
